b7408016f94cf74f8ed2aa91f7f0dac8993f0d18
koobohyeon <goosee115@gmail.com>
2018-05-23 13:06:33 +0900

    게시글 페이지 추가

--- src/index.js
+++ src/index.js
@@ -3,23 +3,41 @@ import axios from "axios";
 const rootEl = document.querySelector(".root");
 const templates = {
   postList: document.querySelector("#post-list").content,
-  postItem: document.querySelector("#post-item").content
+  postItem: document.querySelector("#post-item").content,
+  postContent: document.querySelector("#post-content").content
 };
-
+function render(fragment) {
+  rootEl.textContent = "";
+  rootEl.appendChild(fragment);
+}
 async function indexPage() {
- const res = await axios.get("http://localhost:3000/posts");
- const listFragment = document.importNode(templates.postList, true);
-    res.data.forEach(post => {
-      const fragment = document.importNode(templates.postItem, true);
-      const pEl = fragment.querySelector(".post-item__title");
-      pEl.textContent = post.title;
-      listFragment.querySelector(".post-list").appendChild(fragment);
+  const res = await axios.get("http://localhost:3000/posts");
+  const listFragment = document.importNode(templates.postList, true);
+  res.data.forEach(post => {
+    const fragment = document.importNode(templates.postItem, true);
+    const pEl = fragment.querySelector(".post-item__title");
+    pEl.textContent = post.title;
+    pEl.addEventListener("click", e => {
+      postContentPage(post.id);
     });
-    rootEl.appendChild(listFragment);
+    listFragment.querySelector(".post-list").appendChild(fragment);
+  });
+  render(listFragment)
+}
+
+async function postContentPage(postId) {
+  const res = await axios.get(`http://localhost:3000/posts/${postId}`);
+  const fragment = document.importNode(templates.postContent, true);
+  fragment.querySelector(".post-content__title").textContent = res.data.title;
+  fragment.querySelector(".post-content__body").textContent = res.data.body;
+  fragment.querySelector(".post-content__back-btn").addEventListener('click', e => {
+    indexPage();
+  })
+  render(fragment)
 }
 indexPage();
 // axios.get('http://localhost:3000/posts').then(res => {
-//   const listFragment = document.importNode(document.querySelector('#post-list').content, true);
+//   const listFragment = document햣 .importNode(document.querySelector('#post-list').content, true);
 //   res.data.forEach(post => {
 //     const fragment = document.importNode(document.querySelector('#post-item').content, true);
 //     const pEl = fragment.querySelector('.post-item__title')
